c17c2d99c8551f9419645b45f36a9eacf9335bc0
yarok <y.m.kirsanov@gmail.com>
2026-09-10 21:35:28 +0300

    pgadmin

--- work_gnome/modules/nixos/home/pgsql.nix
+++ work_gnome/modules/nixos/home/pgsql.nix
@@ -1,4 +1,20 @@
-{ config, lib, pkgs, inputs, ... }:
+{ pkgs, ... }:
+
 {
-  services.postgresql.enable = true;
+  services.postgresql = {
+    enable = true;
+
+    # Скрипт, который выполнится один раз при первой инициализации БД
+    initialScript = pkgs.writeText "init-pg-script.sql" ''
+      ALTER USER postgres WITH PASSWORD '1234';
+    '';
+
+    # Разрешаем подключение по паролю для локальной сети/127.0.0.1
+    authentication = pkgs.lib.mkOverride 10 ''
+      # type  database        user            address                 auth-method
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            md5
+      host    all             all             ::1/128                 md5
+    '';
+  };
 }
