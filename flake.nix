7542160cd9acfcef0d5b481ca17e369eeb5f3031
yarok <y.m.kirsanov@gmail.com>
2026-09-13 18:17:37 +0300

    refactored homemanager

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
             ./common/users
             ./common/pkgs
             ./work/modules/desktop-env.nix
-            ./work/modules/rice
+            #./work/modules/rice
           ];
         };
 
